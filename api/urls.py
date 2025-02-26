d159331599f89ef344d4d8df09737d314569b17d
Ali AB <aliabdev07@gmail.com>
2025-02-26 15:39:36 +0000

    Update urls.py
    - add choices list and create path
    - add delete choice path

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("polls/",views.PollListCreate.as_view(), name="polls_list"),
-    path("polls/delete/<int:pk>/",views.PollDelete.as_view(), name="delete_delete"),
+    path("polls/<int:pk>/delete/", views.PollDelete.as_view(), name="delete_poll"),
+    path("choices/",views.ChoiceListCreate.as_view(), name="choice_list"),
+    path("choices/<int:pk>/delete/", views.ChoiceDelete.as_view(), name="delete_choice"),
 ]
