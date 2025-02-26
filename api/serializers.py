8332f57a7e06c5dd7ed5b34253b800c2a476ce3a
Ali AB <aliabdev07@gmail.com>
2025-02-26 00:13:38 +0000

    Update serializers.py
    - Change Username to Email

--- api/serializers.py
+++ api/serializers.py
@@ -1,18 +1,21 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Poll
 
 
+User = get_user_model()
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "password"]
-        # set password to be write only, and never read it when getting user infos
+        fields = ["id", "email", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        print(validated_data)
-        user = User.objects.create_user(**validated_data)
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
         return user
 
 
