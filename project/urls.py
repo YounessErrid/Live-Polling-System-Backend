d1f24a8e884b44db0a72848bee1102f484a099e9
Ali AB <aliabdev07@gmail.com>
2025-03-06 12:55:03 +0000

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -16,14 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/user/register/', CreateUserView.as_view(), name="register"),
+   path('admin/', admin.site.urls),
+    
+    # Authentication endpoints
     path('api/token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
-    path("api-auth/", include("rest_framework.urls")),
+    path("api-auth/", include("rest_framework.urls")),  # Default DRF authentication
+
+    # Include all API endpoints
     path("api/", include("api.urls")),
 ]
