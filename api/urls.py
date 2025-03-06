cee83d9da177fa75be8083dcc245b75274bd235a
Ali AB <aliabdev07@gmail.com>
2025-03-06 12:55:21 +0000

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 from . import views
+from .views import profile_view, register_user
 
 
 urlpatterns = [
-    path("polls/",views.PollListCreate.as_view(), name="polls_list"),
-    path("polls/<int:pk>/delete/", views.PollDelete.as_view(), name="delete_poll"),
-    path("choices/",views.ChoiceListCreate.as_view(), name="choice_list"),
-    path("choices/<int:pk>/delete/", views.ChoiceDelete.as_view(), name="delete_choice"),
+    # User management
+    path("user/register/", register_user, name="register"),
+    path("profile/", profile_view, name="profile"),  # No need for <int:pk>
+
+    # Polls
+    path("polls/", views.PollListCreate.as_view(), name="polls_list"),
+    path("polls/<int:pk>/", views.PollDelete.as_view(), name="poll_detail"),  # RESTful DELETE
+
+    # Choices
+    path("choices/", views.ChoiceListCreate.as_view(), name="choice_list"),
+    path("choices/<int:pk>/", views.ChoiceDelete.as_view(), name="choice_detail"),  # RESTful DELETE
 ]
