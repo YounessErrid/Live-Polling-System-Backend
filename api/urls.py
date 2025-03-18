7f26e42fd073997cf1464ac59a3c8156ce7b4c23
Ali AB <aliabdev07@gmail.com>
2025-03-18 18:10:34 +0000

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
+from .views import get_polls, create_poll, poll_detail, create_choice, choice_detail
 
 
 urlpatterns = [
-    path("polls/",views.PollListCreate.as_view(), name="polls_list"),
-    path("polls/<int:pk>/delete/", views.PollDelete.as_view(), name="delete_poll"),
-    path("choices/",views.ChoiceListCreate.as_view(), name="choice_list"),
-    path("choices/<int:pk>/delete/", views.ChoiceDelete.as_view(), name="delete_choice"),
+    # Polls
+    path("polls/", get_polls, name="get_polls"),
+    path("polls/create", create_poll, name="create_poll"),
+    path("polls/<int:pk>", poll_detail, name="poll_detail"),
+
+    # Choices
+    path("choices/create", create_choice, name="create_choice"),
+    path("choices/<int:pk>", choice_detail, name="choice_detail"),
 ]
